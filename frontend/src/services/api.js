66a575cb8d70229563c3056f47dbacb0be427667
Prabhav Deshpande <prabhavdeshpande89@gmail.com>
2026-03-10 12:57:15 +0530

    fix api url from localhost to render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,12 @@ export const convertFile = async (file) => {
   const response = await axios.post(
     "https://pdf-converter-iu50.onrender.com/convert",
     formData,
-    { responseType: "blob" }
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      responseType: "blob",
+    }
   );
 
   return response.data;
